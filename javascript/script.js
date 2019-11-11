28ec1d3e06d93a3d376043fddfa817900e520f05
Jonathan Gomes <jonathan2gomes.jg@gmail.com>
2019-11-11 20:56:33 +0000

    Question 2

--- javascript/script.js
+++ javascript/script.js
@@ -11,15 +11,26 @@ function sum(){
    var s = n1 + n2;
    
   
-   if ( s > 100 ){
+   if ( s == 100 ){
 
-    res.innerHTML = `O Valor  ${s} eh maior que 100`  ;
+    res.innerHTML = `O Valor  ${s} e igual a 100`;
 
-   } else if ( s == 100) {
+   } else if( s > 100) {
 
-    res.InnerHTML = `O Valor  ${s} eh igual a 100`;
+    res.innerHTML = `O Valor  ${s} e maior do que 100`;
 
     } else {
     res.innerHTML = `O Valor  ${s} eh menor que 100`;
 }
+}
+function mu(){
+    alert('oi')
+    for (var i = 0; i<10; i++){
+        var num = window.document.getElementById('nmult');
+        var resmult = window.document.getElementById('resmult');
+
+        var s = num * i;
+        resnmult.innerHTML = `${num} x ${i} e igual a ${s}`;
+    }
+
 }
